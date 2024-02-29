e3a308af6663999c815e575129487811d8ceab9f
Nkululeko1999 <105264276+Nkululeko1999@users.noreply.github.com>
2024-02-29 15:05:23 +0200

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const app = express();
 
 app.use(cors({
   origin: "https://nkuliappdev.onrender.com",
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], 
+  methods: 'POST', 
   allowedHeaders: ['Content-Type', 'Authorization'],
 }));
 
