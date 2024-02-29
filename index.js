a66645159b8d4cdd3912156c574ac121047027e7
Nkululeko1999 <105264276+Nkululeko1999@users.noreply.github.com>
2024-02-29 15:04:34 +0200

    Update index.js

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use(cors({
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT;
 
 app.post("/api/auto-responder", router);
 
