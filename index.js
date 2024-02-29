74a63669b7b48e50ef62a8bd9e0e3aeaf6307910
Nkululeko1999 <105264276+Nkululeko1999@users.noreply.github.com>
2024-02-29 14:50:35 +0200

    Update index.js
    cors setup

--- index.js
+++ index.js
@@ -8,7 +8,12 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
+app.use(cors(
+  origin: "https://nkuliappdev.onrender.com",
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], 
+  allowedHeaders: ['Content-Type', 'Authorization'],
+));
+
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
